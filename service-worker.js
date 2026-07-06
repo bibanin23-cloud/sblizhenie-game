a28399ca6a630bbbd7b25b42b6a18196e76f6246
bibanin23-cloud <bibanin23@gmail.com>
2026-07-06 22:13:54 +0300

    Add files via upload
    Добавил офлайн режим

---
+++ service-worker.js
@@ -0,0 +1,81 @@
+const CACHE_VERSION = "sblizhenie-pwa-v2";
+const APP_SHELL_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./config.js",
+  "./questions.js",
+  "./app.js",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_VERSION)
+      .then((cache) => cache.addAll(APP_SHELL_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== CACHE_VERSION)
+            .map((cacheName) => caches.delete(cacheName))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(request.url);
+
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const responseCopy = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put("./index.html", responseCopy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(request).then((response) => {
+        if (response && response.ok) {
+          const responseCopy = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put(request, responseCopy));
+        }
+
+        return response;
+      });
+    })
+  );
+});
